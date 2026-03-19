ee2fb58c9f0534845e1b13e0f4f3676c8a6b6562
aliak09 <aliak9901@gmail.com>
2026-03-19 23:51:13 +0500

    Employee dashboard page created

--- app/employee/dashboard/page.tsx
+++ app/employee/dashboard/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function EmployeeDashboard() {
   return (
-    <div className='font-bold text-4xl'>Employee Dashboard</div>
+    <div className='font-bold text-4xl'>Employee Dashboard created</div>
   )
 }
